8b5af346c57378ee187a7cafe70ca3259d737dbc
Vanessa Chu <vanessachu@Vanessas-Air.hitronhub.home>
2017-11-05 19:48:28 -0800

    complete project files

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,6 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var value = 210
+    
+    @IBOutlet var timerValue: UILabel!
+    @IBAction func resetButton(_ sender: Any) {
+        value = 210
+        timerValue.text = String(value)
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        if value - 10 >= 0{
+            value -= 10
+            timerValue.text = String(value)
+        }
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+        value += 10
+        timerValue.text = String(value)
+    }
+    
+    @IBAction func timerPause(_ sender: Any) {
+        if timer.isValid == true{
+            timer.invalidate()
+        }
+    }
+
+    @IBAction func timerPlay(_ sender: Any) {
+
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    func processTimer(){
+        value = Int(timerValue.text!)!
+        if value != 0{
+            value -= 1
+            timerValue.text = String(value)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
